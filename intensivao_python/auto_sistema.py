5e20fbf96ef143f4869de99a0f3292a5838f4df3
José Lucas <jose_lucas77@outlook.com>
2022-03-09 22:52:17 -0300

    update

--- intensivao_python/auto_sistema.py
+++ intensivao_python/auto_sistema.py
@@ -1,4 +1,5 @@
 import pyautogui
+import pyperclip
 import time
 import pandas as pd
 
@@ -22,22 +23,67 @@ def baixar_arquivo():
     time.sleep(3)
 
 
-def base_dados():
+def abrir_email():
+#   Abrir o app do email
+    pyautogui.hotkey('win')
+    pyautogui.write('email')
+    pyautogui.press('enter')
+
+
+def enviar_email():
+#   Base de dados
     tabela = pd.read_excel(r'C:\Users\jose_\Downloads\Vendas - Dez.xlsx')
     faturamento = tabela['Valor Final'].sum()
     quantidade = tabela['Quantidade'].sum()
+    time.sleep(5)
 
-    print(faturamento)
-    print(quantidade)
+#   Abri a janela de escrever o email
+    pyautogui.click(x=81, y=106)
+    time.sleep(5)
+#   Escreve o destinatário
+    pyautogui.write('joselucasa937@gmail.com')
+    pyautogui.press('enter')
+    time.sleep(2)
+
+    pyautogui.press('tab')
+    pyautogui.press('tab')
+    pyautogui.press('tab')
+    time.sleep(2)
+
+#   Escreve o assunto
+    pyautogui.write("Relatorio de vendas")
+    time.sleep(2)
+    pyautogui.press('tab')
+    time.sleep(2)
+
+#   Escrever o corpo do email
+    texto = f'''
+Prezados, bom dia
+
+O faturamento de ontem foi de: R$ {faturamento}
+A quantidade de produtos foi de: R$ {quantidade}
+
+Abs
+JoseLucas
+'''
+    pyperclip.copy(texto)
+    pyautogui.hotkey('ctrl', 'v')
+    time.sleep(2)
+
+#   Enviando o email
+    pyautogui.hotkey('ctrl', 'enter')
+    time.sleep(2)
 
 
 pyautogui.POUSE = 2
 
 abrir_chrome()
-time.sleep(3)
+time.sleep(5)
 entrar_driver()
 time.sleep(5)
 baixar_arquivo()
 time.sleep(5)
-base_dados()
-time.sleep(3)
+abrir_email()
+time.sleep(5)
+enviar_email()
+time.sleep(5)
--- intensivao_python/teste.py
+++ intensivao_python/teste.py
@@ -1,25 +1,38 @@
-import pyautogui
-import time
+from selenium import webdriver # controla o navegador
+from selenium.webdriver.common.keys import Keys # usar o teclado
+from selenium.webdriver.common.by import By # localizar os itens no navegador
 import pandas as pd
 
-def abrir_email():
-    pyautogui.hotkey('win')
-    pyautogui.write('email')
-    pyautogui.press('enter')
+#   criar o navegador
+navegador = webdriver.Chrome()
+#   entraria no google e pesquisa por cotação do dólar
+navegador.get('http://www.google.com/')
+#   pegaria a cotação do dólar
+#   para selecionar um elemento do navegador: f12 ->seleiona o elemento -> copia o XPATH
+navegador.find_element(By.XPATH, '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys('cotação dólar', Keys.ENTER)
+#   pega a cotação do dólar
+cotacao_dolar = navegador.find_element(By.XPATH, '//*[@id="knowledge-currency__updatable-data-column"]/div[1]/div[2]/span[1]').get_attribute('data-value')
+print(cotacao_dolar)
 
+#   entraria no google e pesquisa por cotação euro
+navegador.get('http://www.google.com/')
+#   pegaria a cotação do euro
+navegador.find_element(By.XPATH, '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys('cotação euro', Keys.ENTER)
+#   pega a cotação do euro
+cotacao_euro = navegador.find_element(By.XPATH, '//*[@id="knowledge-currency__updatable-data-column"]/div[1]/div[2]/span[1]').get_attribute('data-value')
+print(cotacao_euro)
 
-def enviar_email():
-    pyautogui.click(x=81, y=106)
-    time.sleep(2)
-    pyautogui.write('joselucasa937@gmail.com')
-    pyautogui.press('enter')
-    time.sleep(2)
-    pyautogui.click(x=672, y=227)
-    time.sleep(2)
+#   entraria no google e pesquisa por cotação do ouro
+navegador.get('http://www.melhorcambio.com/ouro-hoje')
+#   pega a cotação do ouro
+cotacao_ouro = navegador.find_element(By.XPATH, '//*[@id="comercial"]').get_attribute('value')
+cotacao_ouro = cotacao_ouro.replace(",", ".")
+print(cotacao_ouro)
 
-pyautogui.POUSE = 2
+navegador.quit()
 
-abrir_email()
-time.sleep(5)
-enviar_email()
-time.sleep(5)
+tabela = pd.read_excel(r"C:\Users\jose_\Downloads\Produtos.xlsx")
+print(tabela)
+print('\n')
+tabela.loc[tabela['Moeda']=='Dólar', 'cotação'] = float(cotacao_dolar)
+print(tabela)
